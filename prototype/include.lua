b35895f271dde593076a55b494b624b62bd1b4f0
maromaroXD <amine-fb-fb@hotmail.fr>
2020-11-30 04:47:35 +0100

    add include file.

---
+++ prototype/include.lua
@@ -0,0 +1,9 @@
+
+function include()
+  Object  = require "classicc";
+  Ball    = require "ball";
+  Brick   = require "brick";
+  LevelBricks = require "level_bricks";
+  Walls = require "walls";
+  Platform  = require "platform";
+end
--- prototype/main.lua
+++ prototype/main.lua
@@ -1,20 +1,7 @@
--- Platform
-local platform = {};
-platform.pos_x = 500;
-platform.pos_y = 500;
-platform.speed_x = 300;
-platform.width = 70
-platform.height = 20
 
 
 function love.load(arg)
-  Object  = require "classicc";
-  Ball    = require "ball";
-  Brick   = require "brick";
-  LevelBricks = require "level_bricks";
-  Walls = require "walls";
-  Platform  = require "platform";
-
+  include();
   platform  = Platform(500,500,300,70,20);
   ball = Ball(400,400,10,10,10);
   level_one = LevelBricks(50,30,5,11,70,50,10,15);
